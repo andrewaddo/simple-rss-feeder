e1da38cd150118b363e24c93bbf592fb13625ea8
Andrew Do <andrew.addo@gmail.com>
2020-10-14 02:16:57 +0000

    fixed tests; added valid publisher test

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -8,13 +8,20 @@ import os
 WEBHOOK = "https://chat.googleapis.com/v1/spaces/AAAAZjRiWtQ/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=iRwqG6II2ivcnSz6VV_CldAFVBXe2_x_CtVByTTZrag%3D"
 
 def test_publisher_invalidURL():
+    web = webhookPublisher.WebhookPublisher("http://invalidURL.com")
+    content = {
+        'text': 'Hello from a Python script!'}
+    responseStatus = web.publish(dumps(content))
+    print("response status code", responseStatus)
+    assert responseStatus == 404
+
+def test_publisher_validURL():
     web = webhookPublisher.WebhookPublisher(WEBHOOK)
     content = {
         'text': 'Hello from a Python script!'}
     responseStatus = web.publish(dumps(content))
     print("response status code", responseStatus)
-    assert responseStatus == 301
-
+    assert responseStatus == 200
 
 def test_getRSSFeed():
     os.environ['rssURL'] = 'https://cloudblog.withgoogle.com/rss/'
